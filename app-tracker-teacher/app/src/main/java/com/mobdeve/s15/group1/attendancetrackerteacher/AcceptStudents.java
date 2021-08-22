03e6353da3bdc00bd99154a04dd72ad521b34035
William Dimaculangan <william_dimaculangan@dlsu.edu.ph>
2021-08-22 23:36:01 +0800

    Accept student View partially done
    Still havent connected the view tho, will figure out tomorrow

---
+++ app-tracker-teacher/app/src/main/java/com/mobdeve/s15/group1/attendancetrackerteacher/AcceptStudents.java
@@ -0,0 +1,14 @@
+package com.mobdeve.s15.group1.attendancetrackerteacher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AcceptStudents extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accept_students);
+    }
+}
