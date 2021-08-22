27097e38eb9131cc3a7c86fc47b8cd79f8d64bb9
William Dimaculangan <william_dimaculangan@dlsu.edu.ph>
2021-08-22 17:36:36 +0800

    Initialized Student app

---
+++ app-tracker-student/app/src/main/java/com/mobdeve/s15/group1/attendancetrackerstudent/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mobdeve.s15.group1.attendancetrackerstudent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
