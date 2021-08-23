b62396526e54b43d858aafb9c690005b63dbd8ad
enricosebastian <enricosebastiansalazar@gmail.com>
2021-08-23 14:46:48 +0800

    added single meeting views

---
+++ app-tracker-teacher/app/src/main/java/com/mobdeve/s15/group1/attendancetrackerteacher/SingleMeetingView.java
@@ -0,0 +1,14 @@
+package com.mobdeve.s15.group1.attendancetrackerteacher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SingleMeetingView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_meeting_view);
+    }
+}
