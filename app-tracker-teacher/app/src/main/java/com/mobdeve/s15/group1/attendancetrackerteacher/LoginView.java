f5b82b861386308891d1d1b5c5604d19a385ce1a
enricosebastian <enricosebastiansalazar@gmail.com>
2021-08-26 09:28:47 +0800

    added database functionality properly lmao

--- app-tracker-teacher/app/src/main/java/com/mobdeve/s15/group1/attendancetrackerteacher/LoginView.java
+++ app-tracker-teacher/app/src/main/java/com/mobdeve/s15/group1/attendancetrackerteacher/LoginView.java
@@ -1,24 +1,21 @@
 package com.mobdeve.s15.group1.attendancetrackerteacher;
 
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.firestore.FirebaseFirestore;
 
-    //probably some keys
+public class LoginView extends AppCompatActivity {
 
-    EditText inputEmail, inputPassword;
-    Button btnLogin, btnRegister;
+    private EditText inputEmail, inputPassword;
+    private Button btnLogin, btnRegister;
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,17 +23,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         this.btnLogin = findViewById(R.id.btnLogin);
         this.btnRegister = findViewById(R.id.btnRegister);
+
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ClasslistView.class);
+                Intent intent = new Intent(LoginView.this, ClasslistView.class);
                 startActivity(intent);
             }
         });
+
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, RegisterTeacherActivity.class);
+                Intent intent = new Intent(LoginView.this, RegisterTeacherActivity.class);
                 startActivity(intent);
             }
         });
