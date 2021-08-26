4c84356636bb47849da2061ad46913e1b91a4a69
enricosebastian <enricosebastiansalazar@gmail.com>
2021-08-26 09:47:55 +0800

    added firestore references strings

---
+++ app-tracker-teacher/app/src/main/java/com/mobdeve/s15/group1/attendancetrackerteacher/FirestoreReferences.java
@@ -0,0 +1,22 @@
+package com.mobdeve.s15.group1.attendancetrackerteacher;
+
+public class FirestoreReferences {
+
+    public final static String
+        USERS_COLLECTION        = "Users",
+            USERNAME_FIELD      = "username",
+            IDNUMBER_FIELD      = "idNumber",
+            EMAIL_FIELD         = "email",
+            PASSWORD_FIELD      = "password",
+            FIRSTNAME_FIELD     = "firstName",
+            LASTNAME_FIELD      = "lastName",
+            USERTYPE_FIELD      = "userType",
+        COURSES_COLLECTION      = "Courses",
+            COURSECODE_FIELD    = "courseCode",
+            COURSENAME_FIELD    = "courseName",
+            SECTIONCODE_FIELD   = "sectionCode",
+            STUDENTCOUNT_FIELD  = "studentCount",
+            ISPUBLISHED_FIELD   = "isPublished",
+        MEETINGS_COLLECTION     = "Meetings";
+
+}
--- app-tracker-teacher/app/src/main/java/com/mobdeve/s15/group1/attendancetrackerteacher/LoginView.java
+++ app-tracker-teacher/app/src/main/java/com/mobdeve/s15/group1/attendancetrackerteacher/LoginView.java
@@ -4,18 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class LoginView extends AppCompatActivity {
 
     private EditText inputEmail, inputPassword;
     private Button btnLogin, btnRegister;
 
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,9 +29,25 @@ public class LoginView extends AppCompatActivity {
         this.btnLogin = findViewById(R.id.btnLogin);
         this.btnRegister = findViewById(R.id.btnRegister);
 
+        this.db = FirebaseFirestore.getInstance();
+
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                CollectionReference collectionRef = db.collection("Users"); //change to key
+                collectionRef.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        if(queryDocumentSnapshots.isEmpty()) {
+                            Toast.makeText(getApplicationContext(), "this db is empty", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(getApplicationContext(), "this db is not empty", Toast.LENGTH_SHORT).show();
+                            Log.d("collect database ","size is "+queryDocumentSnapshots.size());
+                        }
+                    }
+                });
+
                 Intent intent = new Intent(LoginView.this, ClasslistView.class);
                 startActivity(intent);
             }
