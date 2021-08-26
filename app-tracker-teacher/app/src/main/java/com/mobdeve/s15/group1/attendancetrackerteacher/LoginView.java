630f89d47d0395306620e2378343944bd86d7337
enricosebastian <enricosebastiansalazar@gmail.com>
2021-08-26 11:46:54 +0800

    added better login feature

--- app-tracker-teacher/app/src/main/java/com/mobdeve/s15/group1/attendancetrackerteacher/LoginView.java
+++ app-tracker-teacher/app/src/main/java/com/mobdeve/s15/group1/attendancetrackerteacher/LoginView.java
@@ -15,9 +15,13 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.List;
 
 public class LoginView extends AppCompatActivity {
     private final String TAG = "LoginView.java";
@@ -43,9 +47,10 @@ public class LoginView extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String email = inputEmail.getText().toString();
+                String password = inputPassword.getText().toString();
 
                 CollectionReference collectionRef = db.collection(FirestoreReferences.USERS_COLLECTION);
-                
+
                 Query query = collectionRef.whereEqualTo(FirestoreReferences.EMAIL_FIELD, email);
                 query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
@@ -54,7 +59,18 @@ public class LoginView extends AppCompatActivity {
                             if(task.getResult().isEmpty()) {
                                 Toast.makeText(getApplicationContext(), "dude doesnt exist lmao", Toast.LENGTH_SHORT).show();
                             } else {
-                                Toast.makeText(getApplicationContext(), "dude does exist lmao", Toast.LENGTH_SHORT).show();
+                                QuerySnapshot querySnapshot = task.getResult();
+                                List<DocumentSnapshot> result = querySnapshot.getDocuments();
+                                if(result.get(0).get(FirestoreReferences.USERTYPE_FIELD).toString().equals("teacher")) {
+                                    if(result.get(0).get(FirestoreReferences.PASSWORD_FIELD).toString().equals(password)) {
+                                        Intent intent = new Intent(LoginView.this, ClasslistView.class);
+                                        startActivity(intent);
+                                    } else {
+                                        Toast.makeText(getApplicationContext(), "Wrong password!", Toast.LENGTH_SHORT).show();
+                                    };
+                                } else {
+                                    Toast.makeText(getApplicationContext(), "User is not a teacher", Toast.LENGTH_SHORT).show();
+                                }
                             }
                         } else {
                             Log.d(TAG,"Error getting Users document: "+task.getException());
