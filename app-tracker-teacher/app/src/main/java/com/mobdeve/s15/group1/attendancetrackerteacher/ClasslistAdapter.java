b064cd02d3308291f04fe38e6869710083707bc8
William Dimaculangan <william_dimaculangan@dlsu.edu.ph>
2021-09-06 13:47:08 +0800

    Transgerred popup menu to adapter (unsolved)

--- app-tracker-teacher/app/src/main/java/com/mobdeve/s15/group1/attendancetrackerteacher/ClasslistAdapter.java
+++ app-tracker-teacher/app/src/main/java/com/mobdeve/s15/group1/attendancetrackerteacher/ClasslistAdapter.java
@@ -1,13 +1,19 @@
 package com.mobdeve.s15.group1.attendancetrackerteacher;
 
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -28,8 +34,68 @@ public class ClasslistAdapter extends RecyclerView.Adapter<ClasslistVH> {
     public ClasslistVH onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
         View view = inflater.inflate(R.layout.layout_class, parent, false);
-
         ClasslistVH classlistVH = new ClasslistVH(view);
+
+        classlistVH.setBtnMoreOptionsOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "Action edit course @ position: " + classlistVH.getAdapterPosition());
+                PopupMenu popupMenu = new PopupMenu(v.getContext(), v);
+                popupMenu.inflate(R.menu.course_menu);
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.editCourse:
+                                Log.d(TAG, "Action edit course @ position: " + classlistVH.getAdapterPosition());
+//                                Intent intent = new Intent(itemView.getContext(), EditCourseActivity.class);
+//                                intent.putExtra(Keys.INTENT_COURSECODE, txtClassCode.getText().toString());
+//                                intent.putExtra(Keys.INTENT_SECTIONCODE, txtSectionCode.getText().toString());
+//                                itemView.getContext().startActivity(intent);
+                                return true;
+                            case R.id.deleteCourse:
+                                Log.d(TAG, "Action delete course @ position: " + classlistVH.getAdapterPosition());
+                                new AlertDialog.Builder(classlistVH.itemView.getContext())
+                                        .setTitle("Delete course")
+                                        .setMessage("Are you sure you want to delete this entry?")
+
+                                        // Specifying a listener allows you to take an action before dismissing the dialog.
+                                        // The dialog is automatically dismissed when a dialog button is clicked.
+                                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                                            public void onClick(DialogInterface dialog, int which) {
+//                                                //deletes all info from courses
+//                                                Db.deleteDocument(Db.COLLECTION_COURSES,
+//                                                        Db.FIELD_SECTIONCODE, txtSectionCode.getText().toString(),
+//                                                        Db.FIELD_COURSECODE, txtClassCode.getText().toString()); //DAT NAMING INCONSISTENCY THO LMAO
+//                                                Log.d(TAG,"deleted collection course");
+//
+//                                                //deletes all info from meetings
+//                                                Db.deleteDocument(Db.COLLECTION_MEETINGS,
+//                                                        Db.FIELD_SECTIONCODE, txtSectionCode.getText().toString(),
+//                                                        Db.FIELD_COURSECODE, txtClassCode.getText().toString()); //DAT NAMING INCONSISTENCY THO LMAO
+//                                                Log.d(TAG,"deleted collection meetings");
+////
+////                              //deletes all info from meeting history
+//                                                Db.deleteDocument(Db.COLLECTION_MEETINGHISTORY,
+//                                                        Db.FIELD_SECTIONCODE, txtSectionCode.getText().toString(),
+//                                                        Db.FIELD_COURSECODE, txtClassCode.getText().toString()); //DAT NAMING INCONSISTENCY THO LMAO
+//                                                Log.d(TAG,"deleted collection meeting history");
+
+                                                Toast.makeText(classlistVH.itemView.getContext(), "Course has been successfully deleted", Toast.LENGTH_SHORT).show();
+                                            }
+                                        })
+
+                                        // A null listener allows the button to dismiss the dialog and take no further action.
+                                        .setNegativeButton(android.R.string.no, null).show();
+                                return true;
+                            default:
+                                return false;
+                        }
+                    }
+                });
+            }
+        });
         return classlistVH;
     }
 
@@ -50,6 +116,8 @@ public class ClasslistAdapter extends RecyclerView.Adapter<ClasslistVH> {
                 holder.itemView.getContext().startActivity(singleClassViewIntent);
             }
         });
+
+
     }
 
 
--- app-tracker-teacher/app/src/main/java/com/mobdeve/s15/group1/attendancetrackerteacher/ClasslistVH.java
+++ app-tracker-teacher/app/src/main/java/com/mobdeve/s15/group1/attendancetrackerteacher/ClasslistVH.java
@@ -2,6 +2,7 @@ package com.mobdeve.s15.group1.attendancetrackerteacher;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.media.Image;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,7 +15,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class ClasslistVH extends RecyclerView.ViewHolder implements View.OnClickListener, PopupMenu.OnMenuItemClickListener {
+public class ClasslistVH extends RecyclerView.ViewHolder {
     private static final String TAG = "ClasslistVH.java";
     private TextView txtClassCode, txtSectionCode;
     private ImageButton btnMoreOptions;
@@ -24,7 +25,7 @@ public class ClasslistVH extends RecyclerView.ViewHolder implements View.OnClick
         this.txtClassCode = itemView.findViewById(R.id.txtClassCode);
         this.txtSectionCode = itemView.findViewById(R.id.txtSectionCode);
         this.btnMoreOptions = itemView.findViewById(R.id.btnMoreOption);
-        this.btnMoreOptions.setOnClickListener(this);
+
     }
 
 
@@ -37,85 +38,89 @@ public class ClasslistVH extends RecyclerView.ViewHolder implements View.OnClick
         this.txtSectionCode.setText(txtName);
     }
 
-    @Override
-    public void onClick(View v) {
-        Log.d(TAG, "onCLick: " + getAdapterPosition());
-        showPopupMenu(v);
+    public void setBtnMoreOptionsOnClickListener(View.OnClickListener onClickListener) {
+        this.btnMoreOptions.setOnClickListener(onClickListener);
     }
 
-
-    //  Attribution: https://youtu.be/hKyjb4b19YM
-    private void showPopupMenu(View view)  {
-        PopupMenu popupMenu = new PopupMenu(view.getContext(), view);
-        popupMenu.inflate(R.menu.course_menu);
-        popupMenu.setOnMenuItemClickListener(this);
-        popupMenu.show();
-    }
-
-
-    @Override
-    public boolean onMenuItemClick(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.editCourse:
-                Log.d(TAG, "Action edit course @ position: " + getAdapterPosition());
-                Intent intent = new Intent(itemView.getContext(), EditCourseActivity.class);
-                intent.putExtra(Keys.INTENT_COURSECODE, txtClassCode.getText().toString());
-                intent.putExtra(Keys.INTENT_SECTIONCODE, txtSectionCode.getText().toString());
-                itemView.getContext().startActivity(intent);
-                return true;
-            case R.id.deleteCourse:
-                Log.d(TAG, "Action delete course @ position: " + getAdapterPosition());
-                new AlertDialog.Builder(itemView.getContext())
-                        .setTitle("Delete course")
-                        .setMessage("Are you sure you want to delete this entry?")
-
-                        // Specifying a listener allows you to take an action before dismissing the dialog.
-                        // The dialog is automatically dismissed when a dialog button is clicked.
-                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int which) {
-                                //deletes all info from courses
-                                Db.deleteDocument(Db.COLLECTION_COURSES,
-                                Db.FIELD_SECTIONCODE, txtSectionCode.getText().toString(),
-                                Db.FIELD_COURSECODE, txtClassCode.getText().toString()); //DAT NAMING INCONSISTENCY THO LMAO
-                                Log.d(TAG,"deleted collection course");
-
-                                //deletes all info from meetings
-                                Db.deleteDocument(Db.COLLECTION_MEETINGS,
-                                Db.FIELD_SECTIONCODE, txtSectionCode.getText().toString(),
-                                Db.FIELD_COURSECODE, txtClassCode.getText().toString()); //DAT NAMING INCONSISTENCY THO LMAO
-                                Log.d(TAG,"deleted collection meetings");
+//    @Override
+//    public void onClick(View v) {
+//        Log.d(TAG, "onCLick: " + getAdapterPosition());
+//        showPopupMenu(v);
+//    }
 //
-//                              //deletes all info from meeting history
-                                Db.deleteDocument(Db.COLLECTION_MEETINGHISTORY,
-                                Db.FIELD_SECTIONCODE, txtSectionCode.getText().toString(),
-                                Db.FIELD_COURSECODE, txtClassCode.getText().toString()); //DAT NAMING INCONSISTENCY THO LMAO
-                                Log.d(TAG,"deleted collection meeting history");
-
-                                //how to update recycler view VVVVVVVVVVVVVVVVVV
-                                //how to update recycler view VVVVVVVVVVVVVVVVVV
-                                //how to update recycler view VVVVVVVVVVVVVVVVVV
-                                //how to update recycler view VVVVVVVVVVVVVVVVVV
-                                //how to update recycler view VVVVVVVVVVVVVVVVVV
-
-                                //how to update recycler view ^^^^^^^^^^^^^^^^^^
-                                //how to update recycler view ^^^^^^^^^^^^^^^^^^
-                                //how to update recycler view ^^^^^^^^^^^^^^^^^^
-                                //how to update recycler view ^^^^^^^^^^^^^^^^^^
-                                //how to update recycler view ^^^^^^^^^^^^^^^^^^
-
-                                Toast.makeText(itemView.getContext(), "Course has been successfully deleted", Toast.LENGTH_SHORT).show();
-                            }
-                        })
-
-                        // A null listener allows the button to dismiss the dialog and take no further action.
-                        .setNegativeButton(android.R.string.no, null)
-                        .setIcon(android.R.drawable.ic_dialog_alert)
-                        .show();
-                return true;
-            default:
-                return false;
-        }
-
-    }
+//
+//    //  Attribution: https://youtu.be/hKyjb4b19YM
+//    private void showPopupMenu(View view)  {
+//        PopupMenu popupMenu = new PopupMenu(view.getContext(), view);
+//        popupMenu.inflate(R.menu.course_menu);
+//        popupMenu.setOnMenuItemClickListener(this);
+//        popupMenu.show();
+//    }
+//
+//
+//    @Override
+//    public boolean onMenuItemClick(MenuItem item) {
+//        switch (item.getItemId()) {
+//            case R.id.editCourse:
+//                Log.d(TAG, "Action edit course @ position: " + getAdapterPosition());
+//                Intent intent = new Intent(itemView.getContext(), EditCourseActivity.class);
+//                intent.putExtra(Keys.INTENT_COURSECODE, txtClassCode.getText().toString());
+//                intent.putExtra(Keys.INTENT_SECTIONCODE, txtSectionCode.getText().toString());
+//                itemView.getContext().startActivity(intent);
+//                return true;
+//            case R.id.deleteCourse:
+//                Log.d(TAG, "Action delete course @ position: " + getAdapterPosition());
+//                new AlertDialog.Builder(itemView.getContext())
+//                        .setTitle("Delete course")
+//                        .setMessage("Are you sure you want to delete this entry?")
+//
+//                        // Specifying a listener allows you to take an action before dismissing the dialog.
+//                        // The dialog is automatically dismissed when a dialog button is clicked.
+//                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+//                            public void onClick(DialogInterface dialog, int which) {
+//                                //deletes all info from courses
+//                                Db.deleteDocument(Db.COLLECTION_COURSES,
+//                                Db.FIELD_SECTIONCODE, txtSectionCode.getText().toString(),
+//                                Db.FIELD_COURSECODE, txtClassCode.getText().toString()); //DAT NAMING INCONSISTENCY THO LMAO
+//                                Log.d(TAG,"deleted collection course");
+//
+//                                //deletes all info from meetings
+//                                Db.deleteDocument(Db.COLLECTION_MEETINGS,
+//                                Db.FIELD_SECTIONCODE, txtSectionCode.getText().toString(),
+//                                Db.FIELD_COURSECODE, txtClassCode.getText().toString()); //DAT NAMING INCONSISTENCY THO LMAO
+//                                Log.d(TAG,"deleted collection meetings");
+////
+////                              //deletes all info from meeting history
+//                                Db.deleteDocument(Db.COLLECTION_MEETINGHISTORY,
+//                                Db.FIELD_SECTIONCODE, txtSectionCode.getText().toString(),
+//                                Db.FIELD_COURSECODE, txtClassCode.getText().toString()); //DAT NAMING INCONSISTENCY THO LMAO
+//                                Log.d(TAG,"deleted collection meeting history");
+//
+//                                //how to update recycler view VVVVVVVVVVVVVVVVVV
+//                                //how to update recycler view VVVVVVVVVVVVVVVVVV
+//                                //how to update recycler view VVVVVVVVVVVVVVVVVV
+//                                //how to update recycler view VVVVVVVVVVVVVVVVVV
+//                                //how to update recycler view VVVVVVVVVVVVVVVVVV
+//
+//                                //how to update recycler view ^^^^^^^^^^^^^^^^^^
+//                                //how to update recycler view ^^^^^^^^^^^^^^^^^^
+//                                //how to update recycler view ^^^^^^^^^^^^^^^^^^
+//                                //how to update recycler view ^^^^^^^^^^^^^^^^^^
+//                                //how to update recycler view ^^^^^^^^^^^^^^^^^^
+//
+//                                Toast.makeText(itemView.getContext(), "Course has been successfully deleted", Toast.LENGTH_SHORT).show();
+//                            }
+//                        })
+//
+//                        // A null listener allows the button to dismiss the dialog and take no further action.
+//                        .setNegativeButton(android.R.string.no, null)
+//                        .setIcon(android.R.drawable.ic_dialog_alert)
+//                        .show();
+//                return true;
+//            default:
+//                return false;
+//        }
+//
+//    }
 
 }
